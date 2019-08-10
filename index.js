9a1d1161f2cbbadf1de8438ed0923046dff4db5a
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-08-10 18:03:32 +0200

    added eslint for project

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const Mongoose = require("mongoose");
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
-const chalk = require('chalk');
+const chalk = require("chalk");
  
 Mongoose.connect("mongodb://localhost:27017/nabd",{useNewUrlParser:true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Listening on ${chalk.green(port)}`);
+	console.log(`Listening on ${chalk.green(port)}`);
 });
