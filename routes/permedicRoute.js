405db8b8c15d6beb6c84d11a7af1fc47fd837769
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-08-12 14:36:14 +0200

    document permedic route

--- routes/permedicRoute.js
+++ routes/permedicRoute.js
@@ -4,14 +4,23 @@ const router = express.Router();
 
 require("../models/permedic");
 const permedic = mongoose.model("permedic");
-// const p1 = new permedic({name:"asdas",bio:"asd",rating:50,password:"asdasd",gender:"male",number:"Asdas"});
-// p1.save((err,data)=>{
-// 	console.log("hi");
-// });
+
+/**
+ * Request Permedic
+ * 
+ * @function requestPermedic
+ * @memberof module:models~Permedic
+ * @this module:models~Permedic
+ * 
+ * @param rating the prefered rating for the prequested permedic
+ * @param lastNumber last contacted permedic
+ * 
+ * @return {Permedic} the matched permedic
+ */
 module.exports = router.get("/requestPermedic/:rating/:lastNumber", async (req,res) => {
 	let permedics;
 	// to do add better algorithm using ML if possible
-	await permedic.find({ $and: [ { rating: { $lt: +req.params.rating+5 } }, { rating: {$gt:+req.params.rating-5} } ] },"name",(err,data)=>{
+	await permedic.find({ $and: [ { rating: { $lt: +req.params.rating+30 } }, { rating: {$gt:+req.params.rating-5} } ] },"name",(err,data)=>{
 		permedics = data;
 	}).limit(5);
 	permedics.sort((a, b) => (Math.abs(a.rating-req.rating) > Math.abs(b.rating-req.rating) ? 1 : -1));
