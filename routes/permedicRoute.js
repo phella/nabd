c9a03665e912f8a163cf77485dfc499d0f8e40e3
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-08-18 21:53:28 +0200

    fix permedic route errors

--- routes/permedicRoute.js
+++ routes/permedicRoute.js
@@ -18,28 +18,36 @@ const permedic = mongoose.model("permedic");
  * @return {Permedic} the matched permedic
  */
 module.exports = router.get("/requestPermedic/:rating/:lastNumber", async (req,res) => {
-	let permedics;
-	if(req.rank < 0 || req.rank > 100){
-		res.status(400).json("Rank out of range");
+	let permedics = [];
+	if(req.params.rating < 0 || req.params.rating > 100){
+		return res.status(400).json({"Error":"Rating out of range"});
+	}	
+	if(req.params.lastNumber.length !== 11 || req.params.lastNumber.substring(0,2) !== "01") {
+		return res.status(400).json({"Error":"Wrong phone number format"});
 	}
-	
 	// to do add better algorithm using ML if possible
-	permedics = await permedic.find({ $and: [ { rating: { $lt: +req.params.rating+30 } }, { rating: {$gt:+req.params.rating-5} } ] },"name")
-	.limit(5)
-	.exec();
-	
-	if(permedics === []) {
-		permedics = await permedic.find({},"name")
+	let rating = +req.params.rating ; 
+	while(permedics.length === 0 && rating < 100) {
+		permedics = await permedic.find({ $and: [ { rating: { $lt: rating+30 } }, { rating: {$gt:rating-10} },{available:true} ] },"name number")
+		.limit(5)
 		.exec();
-		if(permedics === []) {
-			res.status(200).json("no available permedics at the moment");
-		}
+		rating += 30;
 	}
-	permedics.sort((a, b) => (Math.abs(a.rating-req.rating) > Math.abs(b.rating-req.rating) ? 1 : -1));
-	if(permedics[0].number !== req.params.lastNumber){
-		res.status(200).json(permedics[0]);
+	rating = +req.params.rating - 10;
+	while(permedics.length === 0 && rating > 0) {
+		permedics = await permedic.find({ $and: [ { rating: { $lt: rating+30 } }, { rating: {$gt:rating-10} },{available : true }] },"name number")
+		.limit(5)
+		.exec();
+		rating -= 10;
+	}
+	if(permedics.length === 0) {
+		return res.status(200).json({"Error" :"No available permedics at the moment"});
+	}
+	permedics.sort((a, b) => (Math.abs(a.rating-req.params.rating) > Math.abs(b.rating-req.params.rating) ? 1 : -1));
+	if(permedics[0].number != req.params.lastNumber || permedics.length === 1 ){
+		return res.status(200).json(permedics[0]);
 	} else {
-		res.status(200).json(permedics[1]);
+		return res.status(200).json(permedics[1]);
 	}
 });
 
