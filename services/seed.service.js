ce3c2104ce84bfca9f49bd051880fc9d38742025
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-08-18 18:37:34 +0200

    add seed service for populating testing DB

---
+++ services/seed.service.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+require("../models/permedic");
+const permedic = mongoose.model("permedic");
+const jwt = require('jsonwebtoken');
+let tempTokens = [];
+
+
+ function populatePermedics(permedics) {
+	permedic.deleteMany({},(err,result)=>{
+		console.log(err);
+		console.log(result);
+	});
+	permedics.forEach(element => {
+		jwt.sign({"phoneNo":element.number,"password":element.password},process.env.SERCETKEY,(err,token)=>{
+			tempTokens.push(token);
+		});
+		let add = new permedic(element);
+		add.save();
+	});
+	// needs refactor
+	 return tempTokens;
+}
+
+module.exports = {populatePermedics};
