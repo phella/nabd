a997c2b0dffb80f86519597b5cd955b50de501de
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-08-19 18:32:53 +0200

    change permedic route logic using bst

--- routes/permedicRoute.js
+++ routes/permedicRoute.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-
+const binarySearchTree = require("../data-structure/bst");
+bst  = new binarySearchTree();
 require("../models/permedic");
 const permedic = mongoose.model("permedic");
 
 /**
  * Request Permedic
- * 
+ * the function uses binary search tree on permedics
  * @function requestPermedic
  * @memberof module:models~Permedic
  * @this module:models~Permedic
@@ -17,38 +18,17 @@ const permedic = mongoose.model("permedic");
  * 
  * @return {Permedic} the matched permedic
  */
-module.exports = router.get("/requestPermedic/:rating/:lastNumber", async (req,res) => {
+module.exports = router.get("/requestPermedic/:rating", async (req,res) => {
 	let permedics = [];
 	if(req.params.rating < 0 || req.params.rating > 100){
 		return res.status(400).json({"Error":"Rating out of range"});
 	}	
-	if(req.params.lastNumber.length !== 11 || req.params.lastNumber.substring(0,2) !== "01") {
-		return res.status(400).json({"Error":"Wrong phone number format"});
-	}
-	// to do add better algorithm using ML if possible
 	let rating = +req.params.rating ; 
-	while(permedics.length === 0 && rating < 100) {
-		permedics = await permedic.find({ $and: [ { rating: { $lt: rating+30 } }, { rating: {$gt:rating-10} },{available:true} ] },"name number")
-		.limit(5)
-		.exec();
-		rating += 30;
+	let permedic = await bst.getPermedic(rating);
+	if(permedic === false){
+		return res.status(200).json({"Error":"No avialable permedics at the moment"});
 	}
-	rating = +req.params.rating - 10;
-	while(permedics.length === 0 && rating > 0) {
-		permedics = await permedic.find({ $and: [ { rating: { $lt: rating+30 } }, { rating: {$gt:rating-10} },{available : true }] },"name number")
-		.limit(5)
-		.exec();
-		rating -= 10;
-	}
-	if(permedics.length === 0) {
-		return res.status(200).json({"Error" :"No available permedics at the moment"});
-	}
-	permedics.sort((a, b) => (Math.abs(a.rating-req.params.rating) > Math.abs(b.rating-req.params.rating) ? 1 : -1));
-	if(permedics[0].number != req.params.lastNumber || permedics.length === 1 ){
-		return res.status(200).json(permedics[0]);
-	} else {
-		return res.status(200).json(permedics[1]);
-	}
+	return res.status(200).json(permedic);
 });
 
 
