7ac8d5274d7f7993eb0f1cda37a1304be1b1219e
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-08-24 21:25:19 +0200

    add 2 more fields

---
+++ routes/patientRoute.js
@@ -0,0 +1,17 @@
+const router = require("./index");
+const patient = require('../models/patient');
+const permedic = require('../models/permedic');
+
+// need to be in another file
+router.get('/welcome/info',async(req,res)=>{
+	let numberPatients,numberPermedics;
+	await patient.countDocuments({},(err,count)=>{
+		numberPatients = count;
+	});	
+	await permedic.countDocuments({},(err,count)=>{
+		numberPermedics = count;
+	});
+	return res.status(200).json({numberPatients,numberPermedics,numberDoctors:0,numberAmbulance:0});
+});
+
+
