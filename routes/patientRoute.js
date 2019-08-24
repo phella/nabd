9941a7f30f05e93dece155beea4400163d6cb9b5
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-08-24 22:33:09 +0200

    change patients to users

--- routes/patientRoute.js
+++ routes/patientRoute.js
@@ -4,7 +4,7 @@ const permedic = require('../models/permedic');
 
 // need to be in another file
 router.get('/welcome/info',async(req,res)=>{
-	let numberPatients;
+	let numberUsers;
 	let numberPermedics;
 	await patient.countDocuments({},(err,count)=>{
 		numberPatients = count;
@@ -12,7 +12,7 @@ router.get('/welcome/info',async(req,res)=>{
 	await permedic.countDocuments({},(err,count)=>{
 		numberPermedics = count;
 	});
-	return res.status(200).json({numberPatients,numberPermedics,numberDoctors:0,numberAmbulance:0});
+	return res.status(200).json({numberUsers,numberPermedics,numberDoctors:0,numberAmbulance:0});
 });
 
 
