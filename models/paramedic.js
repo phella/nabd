ea69deff92a0614995b3b81e4b5a7bb0674620e9
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-08-28 08:29:48 +0200

    fix typo

---
+++ models/paramedic.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+/**
+ * @constructor paramedic
+ * @param {String} name paramedic name
+ * @param {String} bio paramedic bio
+ * @param {Number} rating paramedic rating
+ * @param {String} password paramedic account password
+ * @param {String} gender paramedic gender
+ * @param {String} number paramedic phone number
+ */
+const paramedicSchema = new Schema({
+	name: String,
+	rating: Number,
+	age:Number,
+	password: String,
+	gender: Boolean,
+	phoneNo:String,
+	profilePath: String
+});
+
+module.exports = mongoose.model("paramedic", paramedicSchema);
+
