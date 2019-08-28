c2b0590db47b8c5fee46802b75d2bdb007da5162
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-08-28 08:30:35 +0200

    fix typo paramedic

--- routes/permedicRoute.js
+++ routes/permedicRoute.js
@@ -3,43 +3,43 @@ const express = require("express");
 const router = express.Router();
 const binarySearchTree = require("../data-structure/bst");
 bst  = new binarySearchTree();
-require("../models/permedic");
-const permedic = mongoose.model("permedic");
+require("../models/paramedic");
+const paramedic = mongoose.model("paramedic");
 
 /**
- * Request Permedic
- * the function uses binary search tree on permedics
- * @function requestPermedic
- * @memberof module:models~Permedic
- * @this module:models~Permedic
+ * Request paramedic
+ * the function uses binary search tree on paramedics
+ * @function requestparamedic
+ * @memberof module:models~paramedic
+ * @this module:models~paramedic
  * 
- * @param rating the prefered rating for the prequested permedic
- * @param lastphoneNo last contacted permedic
+ * @param rating the prefered rating for the prequested paramedic
+ * @param lastphoneNo last contacted paramedic
  * 
- * @return {Permedic} the matched permedic
+ * @return {paramedic} the matched paramedic
  */
-module.exports = router.get("/requestPermedic/:rating", async (req,res) => {
+module.exports = router.get("/requestparamedic/:rating", async (req,res) => {
 	if(req.params.rating < 0 || req.params.rating > 100){
 		return res.status(400).json({"Error":"Rating out of range"});
 	}	
 	let rating = +req.params.rating ; 
-	let permedic = await bst.getPermedic(rating);
-	if(permedic === false){
-		return res.status(200).json({"Error":"No avialable permedics at the moment"});
+	let paramedic = await bst.getparamedic(rating);
+	if(paramedic === false){
+		return res.status(200).json({"Error":"No avialable paramedics at the moment"});
 	}
-	return res.status(200).json(permedic);
+	return res.status(200).json(paramedic);
 });
 
 
-router.get("/profile/permedic/:phoneNo" , async (req,res)=>{
+router.get("/profile/paramedic/:phoneNo" , async (req,res)=>{
 	if(req.params.phoneNo.length !== 11 || req.params.phoneNo.substring(0,2) !== "01") {
 		res.status(400).json({"Error":"Wrong phone phoneNo format"});
 	}
-	const profile = await permedic.findOne({phoneNo:req.params.phoneNo},"bio name phoneNo gender profilePath available")
+	const profile = await paramedic.findOne({phoneNo:req.params.phoneNo},"bio name phoneNo gender profilePath available")
 	.cache({"key":req.params.phoneNo})
 	.exec();
 	if(!profile) {
-		res.status(404).json({"Error":"No such permedic"});
+		res.status(404).json({"Error":"No such paramedic"});
 	} else {
 		res.status(200).json(profile);
 	}
