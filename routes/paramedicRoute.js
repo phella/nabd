7cef4fd9e44cc112a0ee5cf02d3770dc3ad42e22
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-09-12 17:34:20 +0200

    replace request paramedic route

--- routes/paramedicRoute.js
+++ routes/paramedicRoute.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-const binarySearchTree = require("../data-structure/bst");
-bst  = new binarySearchTree();
+const util = require('util');
+const redis  = require('redis');
+const redisUrl = "redis://127.0.0.1:6379";
+const socket = require("../services/socket.service");
+const client = redis.createClient(redisUrl);
+client.hget = util.promisify(client.hget);
+// const binarySearchTree = require("../data-structure/bst");
+// bst  = new binarySearchTree();
 require("../models/paramedic");
 const paramedic = mongoose.model("paramedic");
 
@@ -18,6 +24,20 @@ const paramedic = mongoose.model("paramedic");
  * 
  * @return {paramedic} the matched paramedic
  */
+
+module.exports = router.get("/requestParamedic", async (req,res) => {
+	const paramedics = JSON.parse( client.hget("online","paramedic"));
+	if(!paramedics){
+		return res.status(400).json({Error:"No available paramedics"});
+	}
+	const selected = paramedics.splice(0,20);
+	client.hset("online","paramedics",JSON.stringify( paramedics));
+	socket.send(selected,req.body.id); 
+	return res.status(200).json("Requested Successfully");
+});
+
+
+ /*
 module.exports = router.get("/requestparamedic/:rating", async (req,res) => {
 	if(req.params.rating < 0 || req.params.rating > 100){
 		return res.status(400).json({"Error":"Rating out of range"});
@@ -30,7 +50,7 @@ module.exports = router.get("/requestparamedic/:rating", async (req,res) => {
 	return res.status(200).json(paramedic);
 });
 
-
+*/
 router.get("/profile/paramedic/:phoneNo" , async (req,res)=>{
 	if(req.params.phoneNo.length !== 11 || req.params.phoneNo.substring(0,2) !== "01") {
 		res.status(400).json({"Error":"Wrong phone phoneNo format"});
