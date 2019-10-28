780a7285847f43e77cc78fa87b428a101036b007
phella <philopateernabil2021@gmail.com>
2019-10-28 16:55:22 +0200

    add doctor functions for db

--- services/dbManger.js
+++ services/dbManger.js
@@ -1,5 +1,7 @@
 const patient = require("../models/patient");
 const paramedic =require("../models/paramedic");
+const doctor = require("../models/doctor");
+
 async function addPatient(account){
     const result = new patient(account);
     let done = true;
@@ -10,6 +12,29 @@ async function addPatient(account){
     });
     return done;
 }
+
+async function addParamedic(account){
+    const result = new paramedic(account);
+    let done = true;
+    await result.save(function(err){
+        if(err){
+            done = false;
+        }
+    });
+    return done;
+}
+
+async function addDoctor(account){
+    const result = new doctor(account);
+    let done = true;
+    await result.save(function(err){
+        if(err){
+            done = false;
+        }
+    });
+    return done;
+}
+
 async function findPatient(condition,...projectionArr){
 	let result;
 	projectionStr = "";
@@ -34,5 +59,17 @@ async function findParamedic(condition,...projectionArr){
 	return result;
 }
 
-module.exports = {addPatient,findPatient,findParamedic};
+async function findDoctor(condition,...projectionArr){
+	let result;
+	projectionStr = "";
+	projectionArr.forEach(el=>{
+	projectionStr += el + " ";
+	});
+	await doctor.findOne(condition,projectionStr,(err,res)=>{
+		result = res;
+	});
+	return result;
+}
+
+module.exports = {addPatient,findPatient,findParamedic,addParamedic,findDoctor,addDoctor};
 
