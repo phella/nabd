1187ea85274cd6392c9efb47493ecb61baca7b1c
phella <philopateernabil2021@gmail.com>
2019-10-28 18:41:02 +0200

    factor dbmanger

--- services/dbManger.js
+++ services/dbManger.js
@@ -1,6 +1,7 @@
 const patient = require("../models/patient");
 const paramedic =require("../models/paramedic");
 const doctor = require("../models/doctor");
+const ambulance = require("../models/ambulance");
 
 async function addPatient(account){
     const result = new patient(account);
@@ -35,41 +36,44 @@ async function addDoctor(account){
     return done;
 }
 
+async function addAmbulance(account){
+    const result = new ambulance(account);
+    let done = true;
+    await result.save(function(err){
+        if(err){
+            done = false;
+        }
+    });
+    return done;
+}
+
 async function findPatient(condition,...projectionArr){
-	let result;
-	projectionStr = "";
-	projectionArr.forEach(el=>{
-	projectionStr += el + " ";
-	});
-	await patient.findOne(condition,projectionStr,(err,res)=>{
-		result = res;
-	});
-	return result;
+	return await find(patient,condition,projectionArr);
 }
 
 async function findParamedic(condition,...projectionArr){
-	let result;
-	projectionStr = "";
-	projectionArr.forEach(el=>{
-	projectionStr += el + " ";
-	});
-	await paramedic.findOne(condition,projectionStr,(err,res)=>{
-		result = res;
-	});
-	return result;
+	return await find(paramedic,condition,projectionArr);
 }
 
 async function findDoctor(condition,...projectionArr){
+	return await find(doctor,condition,projectionArr);
+}
+
+async function findAmbulance(condition,...projectionArr){
+	return await find(ambulance,condition,projectionArr);
+}
+
+async function find(model,condition,projectionArr){
 	let result;
 	projectionStr = "";
 	projectionArr.forEach(el=>{
 	projectionStr += el + " ";
 	});
-	await doctor.findOne(condition,projectionStr,(err,res)=>{
+	await model.findOne(condition,projectionStr,(err,res)=>{
 		result = res;
 	});
 	return result;
 }
 
-module.exports = {addPatient,findPatient,findParamedic,addParamedic,findDoctor,addDoctor};
+module.exports = {addPatient,addAmbulance,findPatient,findParamedic,addParamedic,findDoctor,addDoctor,findAmbulance,findDoctor};
 
